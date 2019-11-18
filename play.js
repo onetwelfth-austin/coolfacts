bc0b1ddb71afc320c456b6a539567f7de907f8fe
Austin Tucker <austin@1twelfth.com>
2019-11-17 19:23:13 -0500

    added guess verification logic

--- play.js
+++ play.js
@@ -1,6 +1,7 @@
 let utilities = require('./utilities');
 
 //parse answer submission
+//TODO: replace all extra characters in guess and make lower case 
 async function guessAnswer(answer, tableName, docClient, url, token) {
     let answer_variables = utilities.splitMessage(answer);
     let submitter = answer_variables[0];
@@ -20,21 +21,22 @@ async function guessAnswer(answer, tableName, docClient, url, token) {
     };
 
     let results = await utilities.scanTable(docClient, name_parser_params);
-    let userIds = [];
+    let user_ids = [];
     results.forEach((result) => {
-        if (!userIds.includes(result.user_id)) { userIds.push(result.user_id); }
+        if (!user_ids.includes(result.user_id)) { user_ids.push(result.user_id); }
     });
-    userIds.forEach(async (user_id) => {
-        let isCorrect = await checkAnswer(tableName, user_id, guess, docClient);
-        console.log(isCorrect);
-        if (isCorrect) { await updateScore(submitter, tableName, docClient); }
-        alertUser(docClient, submitter, tableName, url, token, isCorrect);
+    user_ids.forEach(async (user_id) => {
+        let outputs = await checkAnswer(tableName, submitter, user_id, guess, docClient);
+        let is_correct = outputs[0];
+        let message = outputs[1];
+        if (is_correct) { await updateScore(submitter, tableName, docClient); }  
+        alertUser(docClient, submitter, tableName, url, token, is_correct, message); 
     });
 }
 
 //check against database to see if that is the right answer.
-//TODO: check against previous guesses to make sure that it has not been guessed correct already 
-async function checkAnswer(tableName, userId, guess, docClient) {
+async function checkAnswer(tableName, submitterUserId, factUserId, guess, docClient) {
+    let output_message;
     let check_answer_params = {
         TableName: tableName,
         FilterExpression: "#ui = :userId and #a = :answer",
@@ -43,13 +45,57 @@ async function checkAnswer(tableName, userId, guess, docClient) {
             "#a": "answer"
         },
         ExpressionAttributeValues: {
-            ":userId": userId,
+            ":userId": factUserId,
             ":answer": guess
         }
     };
     let answers = await utilities.scanTable(docClient, check_answer_params);
-    if (answers.length != 0) { return true; }
-    else { return false; }
+    let is_correct = false;
+    let guessTableName = 'CoolFactsGuesses';
+    if (answers.length != 0) {
+        let check_guess_params = {
+            TableName: guessTableName,
+            FilterExpression: "#gui = :submitterId and #fui = :factUserId and #g = :guess",
+            ExpressionAttributeNames: {
+                "#gui": "guesser_user_id",
+                "#fui": "fact_user_id",
+                "#g": "guess"
+            },
+            ExpressionAttributeValues: {
+                ":submitterId": submitterUserId,
+                ":factUserId": factUserId,
+                ":guess": guess
+            }
+        };
+        let guesses = await utilities.scanTable(docClient, check_guess_params);
+        if (guesses.length != 0) { output_message = 'You have already made that guess! :nerd_face:'; }
+        else { 
+            storeGuess(docClient, guessTableName, submitterUserId, factUserId, guess); 
+            is_correct = true;
+        }
+    }
+    else { output_message = 'You guessed incorrectly. Try again :confused:'; }
+    return [is_correct, output_message];
+}
+
+//store guesses made by players
+function storeGuess(docClient, tableName, submitterUserId, factUserId, guess) {
+    let params = {
+        TableName: tableName,
+        Item: {
+            "guesser_user_id": submitterUserId,
+            "fact_user_id": factUserId,
+            "guess": guess
+        }
+    };
+    console.log("Adding a new item...");
+    docClient.put(params, function (err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
 }
 
 //update score if answer is correct (by id)
@@ -75,7 +121,7 @@ async function updateScore(user, tableName, docClient) {
 }
 
 //alert user of whether they are correct or not
-function alertUser(docClient, user, tableName, url, token, isCorrect) {
+function alertUser(docClient, user, tableName, url, token, isCorrect, outputMessage) {
     if (isCorrect) {
         //get score for user
         let get_score_params = {
@@ -121,17 +167,13 @@ function alertUser(docClient, user, tableName, url, token, isCorrect) {
             qs: {
                 token: token,
                 channel: user_id,
-                text: 'You guessed incorrectly. Try again :confused:'
+                text: outputMessage 
             },
             method: 'POST'
         };
         utilities.makeHTTPRequest(options, () => { }, {});
     }
 }
-
-//create guesses table for each guess
-
-
 
 module.exports = { guessAnswer }
 
